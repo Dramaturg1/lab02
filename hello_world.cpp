0a3c665a34cecb2eb306331cff12eb2289c1c6d8
Dramaturg1 <widowmakerNoOver@outlook.com>
2025-12-04 18:19:09 +0300

    Added comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+//main
 int main() {
     std::string name;
     std::cout << "Enter your name:";
     std::cin >> name;
     std::cout << "hello world from" << name;
     return 0;
-}
+}clea
