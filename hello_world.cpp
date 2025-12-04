72dadffc0d5de5e631fdabed49adf302eff434eb
Dramaturg1 <widowmakerNoOver@outlook.com>
2025-12-04 18:08:06 +0300

    Added user input functionally

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namesapce std;
+
+int main() {
+    string name;
+    cout << "Enter your name:";
+    cin >> name;
+    cout << "hello world from" << name;
+    return 0;
+}
